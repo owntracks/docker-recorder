fbaf94d6948d7f8ecdc2db67be56b1d2f3985080
Giovanni Angoli <juzam76@gmail.com>
2016-11-26 17:08:23 +0100

    rollback upstream PR #9

--- run.sh
+++ run.sh
@@ -8,5 +8,4 @@ docker run -v /tmp/o2:/owntracks -p 11883:1883 -p 18883:8883 -p 8083:8083 \
 	-e MQTTHOSTNAME="${cthostname}" \
 	-e IPLIST="192.168.1.1 127.0.0.83 192.168.1.82" \
 	-e HOSTLIST="foo.example.com bar.org.example.com ${cthostname}" \
-	-e OTR_BROWSERAPIKEY="replace-me" \
 	owntracks/recorderd
